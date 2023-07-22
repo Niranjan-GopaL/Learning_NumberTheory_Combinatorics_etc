da5e7940fd74742fda22e0cbfb973e2ed69459be
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-07-23 00:15:59 +0530

    Q1 => Max gcd done

---
+++ Number_Theory/Max_gcd.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll             long long
+#define fi(a,b)        for(ll i=a; i<b; i++)
+#define fj(a,b)        for(ll j=a; j<b; j++)
+#define pb             push_back
+
+
+// global stuff will be initialized fine
+ll n,m,k,q,x,y,z;
+ll u,v;
+const ll arr_template_size = 1e6;
+
+// global int   arrays are initialized to 0
+ll a[arr_template_size];
+ll b[arr_template_size];
+ll c[arr_template_size];
+
+string s,t;
+ll ans;
+
+
+vector<ll> edges[arr_template_size];
+bool vis[arr_template_size];
+
+
+void solve(){
+    cin >> k;
+    fi(0,k)
+        cin >> a[i];
+    fi(0,k)
+        cout << a[i]/2<< endl;
+}
+
+
+
+int main(){
+    solve();
+}
