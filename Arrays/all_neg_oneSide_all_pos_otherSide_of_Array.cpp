401af4ced4ff21b692d8a994d4b10cbe9dcb70f5
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-08 13:02:42 +0530

    Working through Array manipulation , standard hard problems

---
+++ Arrays/all_neg_oneSide_all_pos_otherSide_of_Array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void printVector(const vector<int>& vec) {
+    for (int elem : vec) 
+        cout << elem << " ";
+    cout << endl;
+}
+
+// g++ sort_0_1_2.cpp && ./a.out   <--- works wonders 
+
+int main() {
+
+    vector<int> myVector = { 2, 2, 2, 0, 1, 1, 0, 0, 2, 2, 2, 2, 0, 0, 1, 0, 1};
+    printVector(myVector);
+
+    int r = myVector.size() - 1 , l = 0 ;
+
+    while(l < r){
+        while (myVector[r] == 2 && r>l ) r-- ;
+        while (myVector[l] != 2 && l<r) l++ ;
+        swap(myVector[l],myVector[r]);
+        printVector(myVector);
+        r--;l++;
+    }
+
+        printVector(myVector);
+        return 0;
+}
---
+++ Arrays/reverse_array_nice.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// reverse array in O(n) time 
+// traversing through only half of the array
+// `AND SPACE`  
+void rvereseArray(int arr[], int start, int end) { 
+    while (start < end) { 
+        swap(arr[start],arr[end]);
+        start++; end--; 
+    }  
+}      
+
+/* Utility function to print an array */
+void printArray(int arr[], int size) 
+{ 
+   for (int i = 0; i < size; i++) 
+   cout << arr[i] << " "; 
+   cout << endl; 
+}  
+
+void recursive_reverse(int arr[], int l, int r){
+    if (l==r) return;
+    if (l>r){
+        swap(arr[l],arr[r]);
+        return;
+    }
+    swap( arr[l], arr[r]);
+    l++; r--;
+    recursive_reverse(arr,l,r);
+} 
+
+
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6}; 
+    printArray(arr, 6); 
+    recursive_reverse(arr, 0, 5); 
+    cout << "Reversed array is" << endl; 
+    printArray(arr, 6); 
+    return 0;
+}
