cfc6608503b26e68716b7bcc425bd35fe7e05324
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-25 00:48:20 +0530

    changed all the function parameters from taking in a string variable to string pointer variable

--- Strings/24_1_string_basics.cpp
+++ Strings/24_1_string_basics.cpp
@@ -17,34 +17,36 @@ template <typename T> const T& max(const T& pA, const T& pB, const T& pC) {
 
 // Damn this worked ?
 // for both reversing a string; checking palindrome,etc ----> TRAVERSE ONLY HALF THE ENTIRE ARRAY
-string reverse_string(string s){
-    int l=0, r=s.length() - 1 ;
+string reverse_string(string* s){
+    int l=0, r = s->length() - 1 ;
     while(l<r){
         /*  I THOUGHT THIS WAS ILLEGAL;  */
         // you can mutate string ??
         swap(s[l],s[r]);
         l++;r--;
     }
-    return s;
+    return *s;
 }
 
-bool is_palindrome(string s){
+bool is_palindrome(string* s){
     // return !s.compare(reverse_string(s)) ;
-    return s == reverse_string(s) ;
+    return (*s) == reverse_string(s) ;
 }
 
 
-bool s1_is_s2_rotated(string s1, string s2){
-    int n = s1.size();
-    if(n != s2.size() )return false;
+bool s1_is_s2_rotated(string* s1, string* s2){
+    int n = s1->size();
+    if(n != s2->size() )return false;
 
     int i,j;
     while(s1[i] != s2[0] )i++ ;
     // now we get the first i at which s1[i] ==s2[0]
+    //  [------------i--]
+    //  [0--------------]
 
     for(int j=1;j<n-1;j++ ){
-        //  [------------i--]
-        //  [-j-------------]
+        //  [------------i+1--]
+        //  [-1--------------]
         // so I need to do circular rotation for i; hence MOD N; really nice idea
         if( s1[(i+1)%n] != s2[j]  ) return false;
         i++;
@@ -52,16 +54,21 @@ bool s1_is_s2_rotated(string s1, string s2){
     return true;
 }
 
+bool has_duplicate(string* s){
+    
+}
 
 
 int main(){
     string s; cin >> s;
 
-    // cout << reverse_string(s) << "\n" ;
-    // cout << is_palindrome(s)  << "\n" ;
+    // cout << reverse_string(&s) << "\n" ;
+    // cout << is_palindrome(&s)  << "\n" ;
     
-    string s2; cin >> s2;
-    cout << s1_is_s2_rotated(s,s2) << "\n" ;
+    // string s2; cin >> s2;
+    // cout << s1_is_s2_rotated(&s,&s2) << "\n" ;
+
+
 
     return 0;
 }
