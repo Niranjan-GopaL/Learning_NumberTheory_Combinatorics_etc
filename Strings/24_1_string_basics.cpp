3310955d2054415edda375c578ef14026c2a35cc
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-24 23:52:48 +0530

    string operations started

---
+++ Strings/24_1_string_basics.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long     ll ;      
+typedef vector<int>    vi ;
+typedef pair<int,int> pi ;
+
+#define pb      push_back
+#define mp      make_pair
+#define f       first
+#define s       second
+#define len(A)  sizeof(A) / sizeof(A[0])
+
+template <typename T> const T& max(const T& pA, const T& pB, const T& pC) { 
+    return max(pA, max(pB, pC)); 
+}
+
+// Damn this worked ?
+string reverse_string(string s){
+    int l=0, r=s.length() - 1 ;
+    while(l<r){
+        /*  I THOUGHT THIS WAS ILLEGAL;  */
+        // you can mutate string ??
+        swap(s[l],s[r]);
+        l++;r--;
+    }
+    return s;
+}
+
+
+
+
+int main(){
+    string s; cin >> s;
+    cout << reverse_string(s) ;
+    return 0;
+}
