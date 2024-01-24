50775f45a6746ba41cd947535794e6d17af78e4b
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-24 23:53:34 +0530

    Started matrix operations

---
+++ Matrix/24_1_matrix_basics.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long     ll ;      
+typedef vector<int>    vi ;
+typedef pair<int,int> pi ;
+
+#define pb      push_back
+#define mp      make_pair
+#define f       first
+#define s       second
+#define len(A)  sizeof(A) / sizeof(A[0])
+
+template <typename T> const T& max(const T& pA, const T& pB, const T& pC) { 
+    return max(pA, max(pB, pC)); 
+}
+
+
+
+// invert array and vector { since same logic; but to see how we implement it; }
+
+vi rotate_by_90(vector<vector<int>>* v){
+    
+}
+
+
+
+
+
+
+int main(){
+
+    return 0;
+}
