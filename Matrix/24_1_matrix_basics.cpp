4f931f801ff9993504b4e8f5f1d352f91dca3769
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-27 01:54:20 +0530

    Spiral matrix traversal done

--- Matrix/24_1_matrix_basics.cpp
+++ Matrix/24_1_matrix_basics.cpp
@@ -24,11 +24,50 @@ vi rotate_by_90(vector<vector<int>>* v){
 }
 
 
+/*
+  [ [- - - -  ]
+    [- - - -  ]
+    [- - - -  ] ]
 
+
+*/
+void spiralTraversal(vector<vector<int>>& matrix) {
+    int top = 0, bottom = matrix.size() - 1;
+    int left = 0, right = matrix[0].size() - 1;
+
+    while (top <= bottom && left <= right) {
+        // Traverse top row
+        for (int i = left; i <= right; ++i) cout << matrix[top][i] << " ";
+        top++;
+
+        // Traverse right column
+        for (int i = top; i <= bottom; ++i) cout << matrix[i][right] << " ";
+        right--;
+
+        // Traverse bottom row
+        if (top <= bottom) {
+            for (int i = right; i >= left; --i) cout << matrix[bottom][i] << " ";
+            bottom--;
+        }
+
+        // Traverse left column
+        if (left <= right) {
+            for (int i = bottom; i >= top; --i) cout << matrix[i][left] << " ";
+            left++;
+        }
+    }
+}
 
 
 
 int main(){
+    vector<vector<int>> matrix = {
+        { 1, 2 , 3,  4 },
+        { 5, 6 , 7,  8 },
+        { 9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
 
+    spiralTraversal(matrix);
     return 0;
 }
