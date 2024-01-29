0b9c0f3dc68ca164544a69823facb3db425cd306
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-29 23:48:06 +0530

    Binomial coeff recursievly

---
+++ DP/binomial_coeff.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+#define n 10
+
+
+// ALways think how to model this as a a recursive definition for DP
+
+// C(n, r) = C(n-1, r-1) + C(n-1, r)
+// C(n, 0) = C(n, n) = 1 { Base case }
+// But we can optimise this by using DP
+
+
+
+
+int main(){
+
+
+}
