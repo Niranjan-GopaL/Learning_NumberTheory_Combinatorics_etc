45e067fd4c0ecc8e355a5c7805beb0b370a54961
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-30 16:07:18 +0530

    My list of all custom templates

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long     ll ;      
+typedef vector<int>   vi ;
+typedef pair<int,int> pi ;
+
+#define pb      push_back
+#define mp      make_pair
+#define f       first
+#define s       second
+#define len(A)  sizeof(A) / sizeof(A[0])
+
+template <typename T>  T& max( T& pA,  T& pB,  T& pC) { 
+    return max(pA, max(pB, pC)); 
+}
+
+
+// Contains stuff that I made for myself ; 
+// will be updated frequently as I get more and more understanding of the subject ;
