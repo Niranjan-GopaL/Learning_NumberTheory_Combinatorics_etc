eecc0c4b18d4c1e2da8e853d885b0f7e0af34eb1
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-30 23:30:05 +0530

    LCS still figuring out a way; now I can generate random strings ; figured out why I had hgihlighted % at the end of output (inserted by zsh cuz I didn't do a newline at the end of stdout

--- DP/29_1_Longest_Common_subsequence.cpp
+++ DP/29_1_Longest_Common_subsequence.cpp
@@ -2,13 +2,27 @@
 #include <random>
 using namespace std;
 
-#define n 10
-
-int LCS(int a[], int b[]){
-
-    
+#define n 10 
+#define m 10 
 
 
+// X ----> len = m
+// Y ----> len = n
+int LCS(string x, string y){
+    // lcs[i][j]  ----> gives the lcs of x[0...i-1] y[0...j-1]
+    int lcs[m+1][n+1];
+
+    for(int i=0; i<=m; i++){
+        for(int j=0; j<=n; j++){
+            if(i==0 || j==0) lcs[i][j] = 0;
+
+            else if ( x[i-1] == y[j-1] ) lcs[i][j] = lcs[i-1][j-1] + 1 ;
+
+            else lcs[i][j] = max( lcs[i-1][j], lcs[i][j-1] ) ;
+        }
+    }
+
+    return  lcs[m][n] ;
 }
 
 
@@ -16,10 +30,15 @@ int LCS(int a[], int b[]){
 
 
 int main(){
+    string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(1, 100);
-    int a[n];
-    for (int i = 0; i < n; ++i) a[i] = dis(rd) ;    
+    uniform_int_distribution<> dis(0, charset.length() - 1);   
+
+    string random_string; random_string.reserve(n);
+    for (int i = 0; i < n; ++i)  random_string += charset[ dis(gen) ];
+
+    cout << random_string << "\n";
+    // cout << random_string ;
 
 }
