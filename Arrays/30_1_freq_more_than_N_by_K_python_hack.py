a8259926a5a77e5bd362de88316f9ddf2fd3735e
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-30 23:28:01 +0530

    Python maigc

---
+++ Arrays/30_1_freq_more_than_N_by_K_python_hack.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+
+def printElements(arr, n, k):
+	x = n//k
+	# Counting frequency of every element using Counter
+	mp = Counter(arr)
+	[print(it) for it in mp if mp[it] > x]
+
+if __name__ == '__main__':
+	arr = [1, 1, 2, 2, 3, 5, 4, 2, 2, 3, 1, 1, 1]
+	n = len(arr); k = 4
+	printElements(arr, n, k)
