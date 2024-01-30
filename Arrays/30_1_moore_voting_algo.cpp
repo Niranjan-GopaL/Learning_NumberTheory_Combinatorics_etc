3c9080969a59d5e3302a43d4fa556e327b91f172
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-01-30 23:28:41 +0530

    The harder varient of more than N/K times

--- Arrays/30_1_moore_voting_algo.cpp
+++ Arrays/30_1_moore_voting_algo.cpp
@@ -54,21 +54,77 @@ int get_majority_elem_(int *a, int n){
     }
     return res;
 
-// My way and IT ALWAYS require soo much clutter 
-
-/*
+/* My way and IT ALWAYS require soo much clutter 
     int res = a[0], c = 1;
     for(int i=1; i< n; i++){
         if( a[i] == res)c++;
         else{
             if(!c){res = a[i]; c = 1;}
             else c-- ;
-        }
-    }
-*/   
-
+    }}*/   
 }
 
+
+// ### Find all elements that appear more than n/k times
+// Harder varient using same idea
+// O(N*K) time complexity and   O(k) space   (for small k this is very efficient)
+int get_n_k_majority_elem(int* a, int n, int k){
+
+}
+// A structure to store an element and its current count
+struct eleCount {
+    int e; // Element
+    int c; // Count
+};
+
+void moreThanNdK(int arr[], int n, int k)
+{
+    // k must be greater than 1 to get some output
+    if (k < 2)
+        return;
+ 
+    /* Step 1: Create a temporary array (contains element and count) of size k-1. Initialize count of all elements as 0 */
+    struct eleCount temp[k - 1];
+    for (int i = 0; i < k - 1; i++)
+        temp[i].c = 0;
+ 
+    /* Step 2: Process all elements of input array */
+    for (int i = 0; i < n; i++) {
+        int j;
+        /* If arr[i] is already present in the element count array, 
+        then increment its count */
+        for (j = 0; j < k - 1; j++) {
+            if (temp[j].e == arr[i]) { temp[j].c += 1; break; }
+        }
+ 
+        /* If arr[i] is not present in temp[] */
+        if (j == k - 1) {
+            int l;
+            /* If there is position available in temp[], then place arr[i] in the first available position and set count as 1*/
+            for (l = 0; l < k - 1; l++) {
+                if (temp[l].c == 0) {temp[l].e = arr[i]; temp[l].c = 1; break; }
+            }
+            /* If all the position in the temp[] are filled, then decrease count of every element by 1 */
+            if (l == k - 1) for (l = 0; l < k - 1; l++) temp[l].c -= 1;
+        }
+    }
+ 
+    /*Step 3: Check actual counts of potential candidates in temp[]*/
+    for (int i = 0; i < k - 1; i++) {
+        // Calculate actual count of elements
+        int ac = 0; // actual count
+        for (int j = 0; j < n; j++)
+            if (arr[j] == temp[i].e)
+                ac++;
+ 
+        // If actual count is more than n/k,
+        // then print it
+        if (ac > n / k)
+            cout << "Number:" << temp[i].e
+                 << " Count:" << ac << "\n";
+    }
+}
+ 
 
 
 
