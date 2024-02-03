f78718244d97241b2e7c9c1a41a1bd0febe60909
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-02-03 22:51:50 +0530

    Debugged both the codes ; code to DETECT cycle using two pointers

--- LinkedList/3_2_ll_detect_cycle.cpp
+++ LinkedList/3_2_ll_detect_cycle.cpp
@@ -22,32 +22,20 @@ nullptr is the preferred way to represent a null pointer, and it was introduced 
 It provides type safety and is INTENDED TO BE USED IN PLACE OF NULL */
 struct Node {
     int data; Node* next;
-    Node(int value) : data(value), next(nullptr) {}
-    // this is also equally valid
-    // Node(int data) { this->data = data; next = nullptr; }
+    // Node(int value) : data(value), next(nullptr) {}
+    Node(int data) { this->data = data; next = nullptr; }
 };
 
 
-Node* reverse_ll_Iterative(Node* head) {
-    Node* prev = nullptr; Node* current = head; Node* next = nullptr;
-
-    while (current != nullptr) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+bool is_cyclic(Node* head){
+    Node *slow = head, *fast = head;
+    while(fast != nullptr){
+        cout << "SLOW : " << slow->data << "  FAST : "<< fast->data << "\n" ;
+        slow = slow->next ;
+        fast = fast->next->next ;
+        if(slow == fast ) return true;
     }
-    head = prev; return head;
-}
-
-Node* reverse_ll_recursive(Node* head) {
-    if (head == nullptr || head->next == nullptr)  return head;
-
-    Node* rest = reverse_ll_recursive(head->next);
-    head->next->next = head;
-    head->next = nullptr;
-
-    return rest;
+    return false;
 }
 
 
@@ -65,15 +53,13 @@ int main() {
     head->next = new Node(2);
     head->next->next = new Node(3);
     head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    head->next->next->next->next->next = new Node(6);
+    head->next->next->next->next->next->next = head;
 
-    cout << "Original List: ";
-    printList(head);
-
-    head = reverse_ll_Iterative(head);
-    head = reverse_ll_recursive(head);
-
-    cout << "Reversed List: ";
-    printList(head);
+    // printList(head);
+    bool ans = is_cyclic(head);
+    cout << "\nAnswer : " << ans << "\n" ;
 
     return 0;
 }
