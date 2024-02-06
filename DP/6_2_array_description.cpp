9089d4370df74292e80d4d663ec3a2840b01460f
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-02-06 23:48:06 +0530

    Homework Problems initialisations

---
+++ DP/6_2_array_description.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long        ll ;      
+typedef vector<int>      vi ;
+typedef pair<int,int>    ii ;
+
+#define pb      push_back
+#define mp      make_pair
+#define f       first
+#define s       second
+#define len(A)  sizeof(A) / sizeof(A[0])
+
+int solve(int a){
+
+}
+
+
+int main(){
+    
+}
