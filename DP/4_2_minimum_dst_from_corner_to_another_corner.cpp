1a378abe93886fed3bb148a3470dae37554c6d05
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-02-06 23:34:21 +0530

    finished grid problems to understand states and transitions better

--- DP/4_2_minimum_dst_from_corner_to_another_corner.cpp
+++ DP/4_2_minimum_dst_from_corner_to_another_corner.cpp
@@ -15,14 +15,32 @@ template <typename T>  T& max( T& pA,  T& pB,  T& pC) {
     return max(pA, max(pB, pC)); 
 }
 
+#define n 100
+#define m 100
+#define inf 2e5
 
+int dp[n][m];
 
+int grid[n][m];
+
+int f(int i, int j){
+    if(i>=n && j>=m)return inf; // moving outside the grid isn't allowed
+
+    if(i==n-1 && j==m-1)return grid[n-1][m-1]; // reached destination
+
+    // if the state is already calculated then return it from mem
+    if(dp[i][j]!=-1)return dp[i][j];
+
+    // state never seen before
+    dp[i][j] = grid[i][j] + max(f(i,j+1), f(i+1,j)) ;
+    return dp[i][j];
+}
 
 
 int solve(int a){
-
+    for(int i=0; i<n; i++)for(int j=0;j<m;j++) dp[i][j] = -1 ;
+    cout << f(0,0) << "\n" ;
 }
-
 
 
 
