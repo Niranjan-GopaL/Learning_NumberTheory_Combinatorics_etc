f891aad5787f468dbb4060f434414d2cb340a448
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-02-08 21:26:47 +0530

    cin cout faster template

--- template.cpp
+++ template.cpp
@@ -15,6 +15,24 @@ template <typename T>  T& max( T& pA,  T& pB,  T& pC) {
     return max(pA, max(pB, pC)); 
 }
 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // a bit faster 
+    int a, b;
+    scanf("%d %d", &a, &b); 
+    printf("%d %d\n", a, b);
+
+    string s,x;
+    getline(cin, s); 
+    while (cin >> x) { /*code*/ }
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout); // now use cin and cout
+
+     long long y = 123456789123456789LL;
+}
 
 // Contains stuff that I made for myself ; 
 // will be updated frequently as I get more and more understanding of the subject ;
