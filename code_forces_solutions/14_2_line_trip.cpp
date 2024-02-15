49cde021a4ffbc3e9e438cd187a98a4f74598967
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-02-15 10:54:48 +0530

    How TF is this having a worse time complexity

--- code_forces_solutions/14_2_line_trip.cpp
+++ code_forces_solutions/14_2_line_trip.cpp
@@ -3,18 +3,25 @@ using namespace std;
 
 typedef long long ll ;
 
-int a[10000];
 
-void failed(){
+
+
+
+// 15 ms   0KB
+int a[10000];
+void initial_try(){
     int t,n,x,m=-1; cin >> t;
     while(t--){
-        cin >> n >> x; cin >> a[0] ;
-        for(int i=1; i<n; i++){ cin >> a[i]; m = max(m, a[i]-a[i-1]) ;}
+        cin >> n >> x;
+        for(int i=0; i<n; i++)cin >> a[i];
+        for(int i=1; i<n; i++) m = max(m, a[i]-a[i-1]) ;
+        m = max( m , a[0] - 0) ; m = max( m , (x-a[n-1]) * 2 ) ;
         cout << m << "\n";
-        m = max( m , x-a[n-1]*2 ) ;
         m = -1 ;
     }
 }
+
+
 /*
 3
 3 7
@@ -25,17 +32,24 @@ void failed(){
 7
 */
 
-int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-    int t,n,x,m=-1; cin >> t;
+
+// 32ms 0KB
+void how_is_this_worse(){
+        int t,n,r,l,x,m=-1; cin >> t;
     while(t--){
         cin >> n >> x;
-        for(int i=0; i<n; i++)cin >> a[i];
-        for(int i=1; i<n; i++) m = max(m, a[i]-a[i-1]) ;
-        m = max( m , a[0] - 0) ;
-        m = max( m , (x-a[n-1]) * 2 ) ;
+        r=0;l=0;
+        for(int i=0; i<n; i++){ cin >> r; m = max(m, r-l) ; l = r; }
+        m = max( m , (x-l)*2 ) ;
         cout << m << "\n";
         m = -1 ;
     }
+}
 
+
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    // initial_try();
+    how_is_this_worse() ; 
 }
