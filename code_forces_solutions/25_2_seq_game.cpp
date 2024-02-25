88f7db391ddc2f458d41159ce14abae2e1d37966
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2024-02-25 22:01:30 +0530

    99% done; some small bug

---
+++ code_forces_solutions/25_2_seq_game.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef long long        ll ;      
+#define pb push_back
+
+
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    ll t; cin >> t;
+    while(t--){
+        ll n,x,temp; cin >> n; temp=n;
+        vector<int> b;
+        n--;
+        cin >> x; b.pb(x);
+        while(n--){cin >> x; b.pb(x); b.pb(x); }
+        cout << 2*temp-1 << "\n";
+        for(int i=0;i< 2*temp-1;i++){ cout << b[i] << " "; } 
+        cout << "\n";
+    }
+}
